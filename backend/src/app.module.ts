7f5411a6e3d785a7f9bb472e80e59f1ed12a51c6
Cesar Garces <cesaringg271404@gmail.com>
2026-05-03 22:21:16 -0600

    Feature: Actualizacion en la carpeta de backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,15 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CitasModule } from './citas/citas.module';
-import { ServiciosModule } from './servicios/servicios.module';
 import { PagesModule } from './pages/pages.module';
-import { DashboardModule } from './dashboard/dashboard.module';
 import { ServicesModule } from './services/services.module';
 import { DashboardsModule } from './dashboards/dashboards.module';
 import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [AuthModule, CitasModule, ServiciosModule, PagesModule, DashboardModule, ServicesModule, DashboardsModule, ProfileModule],
+  imports: [AuthModule, CitasModule, PagesModule, ServicesModule, DashboardsModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
