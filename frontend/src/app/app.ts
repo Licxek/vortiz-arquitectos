e833c4c75f5b6e56ec6e103a8e8c7ba2a72463fd
Cesar Garces <cesaringg271404@gmail.com>
2026-05-11 09:28:41 -0600

    Feature: Cambios en el frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Vortiz_Arquitectos');
+  protected readonly title = signal('HOLA PUTITA');
 }
